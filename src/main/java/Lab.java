75fae67c320df19fbcba96d893dc4be82441fd6e
Filmon M Habtu <filsolu93@gmail.com>
2023-02-03 06:39:37 +0000

    Coding Lab: Comparisons copleted

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,54 +18,24 @@
  * boolean comparison operators here: https://www.oreilly.com/library/view/java-for-dummies/9781118239742/a17.html
  */
 public class Lab {
-    /**
-     * @param a the first number to be compared.
-     * @param b the second number to be compared.
-     * @return true if a is equal to b, false otherwise.
-     */
     public boolean isEqual(int a, int b){
-        return false;
+        return a == b;
     }
 
-    /**
-     * @param a the first number to be compared.
-     * @param b the second number to be compared.
-     * @return true if a is greater than b, false otherwise.
-     */
     public boolean greaterThan(int a, int b){
-        return false;
+        return a > b;
     }
-    /**
-     * @param a the first number to be compared.
-     * @param b the second number to be compared.
-     * @return true if a is less than b, false otherwise.
-     */
     public boolean lessThan(int a, int b){
-        return false;
+        return a < b;
     }
-    /**
-     * @param a the first number to be compared.
-     * @param b the second number to be compared.
-     * @return true if a is greater than or equal to b, false otherwise.
-     */
     public boolean greaterThanOrEqualTo(int a, int b){
-        return false;
+        return a >= b;
     }
-    /**
-     * @param a the first number to be compared.
-     * @param b the second number to be compared.
-     * @return true if a is less than or equal to b, false otherwise.
-     */
     public boolean lessThanOrEqualTo(int a, int b){
-        return false;
+        return a <= b;
     }
 
-    /**
-     * @param a the first number to be compared.
-     * @param b the second number to be compared.
-     * @return true if a is not equal to b, false otherwise.
-     */
     public boolean notEqualTo(int a, int b){
-        return false;
+        return a != b;
     }
 }
